eb295ce747d21996018d6cdf42b148f34f2ed1d0
Chris Russell <cgrussell@gmail.com>
2024-04-07 07:49:15 +0100

    Pulled helper method into FileSystemanAnalyzer class

--- src/CodeGraph.Domain/Dotnet/Analyzers/FileSystemAnalyzer.cs
+++ src/CodeGraph.Domain/Dotnet/Analyzers/FileSystemAnalyzer.cs
@@ -1,4 +1,6 @@
 ﻿using Buildalyzer;
+using CodeGraph.Domain.Graph.Nodes;
+using CodeGraph.Domain.Graph.Triples;
 using CodeGraph.Domain.Graph.Triples.Abstract;
 using Microsoft.CodeAnalysis;
 
@@ -6,9 +8,14 @@ namespace CodeGraph.Domain.Dotnet.Analyzers
 {
     public class FileSystemAnalyzer
     {
+        /// <summary>
+        ///     Returns list of triples for entire list of projects
+        /// </summary>
+        /// <param name="projects"></param>
+        /// <returns></returns>
         public IEnumerable<Triple> FileSystemTriplesFromProjects(List<(Project, IProjectAnalyzer)> projects)
         {
-            var allDocumentFilePaths = projects
+            List<string?> allDocumentFilePaths = projects
                 .Select(tuple => tuple.Item1)
                 .Select(p => p.Documents)
                 .SelectMany(documents => documents.Select(document => document.FilePath))
@@ -16,11 +23,63 @@ namespace CodeGraph.Domain.Dotnet.Analyzers
                 .Where(filePath => !filePath!.Contains("obj"))
                 .ToList();
 
-            foreach (var filepath in allDocumentFilePaths)
+            foreach (string? filepath in allDocumentFilePaths)
             {
-                foreach (var triple in FileSystemHelpers.GetFileSystemChain(filepath!).ToList())
+                foreach (Triple triple in GetFileSystemChain(filepath!).ToList())
                 {
                     yield return triple;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Returns list of triples representing file system path
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public IEnumerable<Triple> GetFileSystemChain(string filePath)
+        {
+            string[] chain = filePath.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries);
+
+            string fileName = Path.GetFileName(filePath);
+
+            FolderNode? prevNode = default;
+            for (int i = 0; i < chain.Length; i++)
+            {
+                TripleIncludedIn triple;
+                int rangeEnd = i + 1;
+
+                if (i < chain.Length - 1)
+                {
+                    FolderNode currNode = new FolderNode(Path.Combine(chain[..rangeEnd]), chain[i]);
+
+                    if (prevNode != default)
+                    {
+                        triple = new TripleIncludedIn(currNode, prevNode);
+                        yield return triple;
+                    }
+
+                    prevNode = currNode;
+                }
+
+                else if (i == chain.Length - 1 && fileName == chain[i])
+                {
+                    FileNode currNode = new FileNode(Path.Combine(chain[..rangeEnd]), chain[i]);
+
+                    if (prevNode == default)
+                    {
+                        continue;
+                    }
+
+                    triple = new TripleIncludedIn(currNode, prevNode);
+                    yield return triple;
+                }
+
+                else
+                {
+                    Console.Error.WriteLine($"{i} : {chain[i]} : {filePath}");
+                    throw new ArgumentException("Something went wrong figuring out file system chain", filePath);
                 }
             }
         }
--- src/CodeGraph.Domain/Dotnet/Analyzers/FileSystemHelpers.cs
+++ src/CodeGraph.Domain/Dotnet/Analyzers/FileSystemHelpers.cs
@@ -28,53 +28,6 @@ namespace CodeGraph.Domain.Dotnet.Analyzers
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        ///     Returns list of triples representing file system path
-        /// </summary>
-        /// <param name="filePath"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentException"></exception>
-        public static IEnumerable<Triple> GetFileSystemChain(string filePath)
-        {
-            var chain = filePath.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries);
-
-            var fileName = Path.GetFileName(filePath);
-
-            FolderNode? prevNode = default;
-            for (var i = 0; i < chain.Length; i++)
-            {
-                TripleIncludedIn triple;
-                var rangeEnd = i + 1;
-
-                if (i < chain.Length - 1)
-                {
-                    var currNode = new FolderNode(Path.Combine(chain[..rangeEnd]), chain[i]);
-
-                    if (prevNode != default)
-                    {
-                        triple = new TripleIncludedIn(currNode, prevNode);
-                        yield return triple;
-                    }
-
-                    prevNode = currNode;
-                }
-
-                else if (i == chain.Length - 1 && fileName == chain[i])
-                {
-                    var currNode = new FileNode(Path.Combine(chain[..rangeEnd]), chain[i]);
-
-                    if (prevNode == default) continue;
-                    
-                    triple = new TripleIncludedIn(currNode, prevNode);
-                    yield return triple;
-                }
-
-                else
-                {
-                    Console.Error.WriteLine($"{i} : {chain[i]} : {filePath}");
-                    throw new ArgumentException("Something went wrong figuring out file system chain", filePath);
-                }
-            }
-        }
+       
     }
 }
