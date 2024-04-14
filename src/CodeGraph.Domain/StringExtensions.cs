27da923040b44b3430a62cb47c20b2fe1cd3f30a
Chris Russell <cgrussell@gmail.com>
2024-04-14 22:16:51 +0100

    Extension method to split on capitals with tests

---
+++ src/CodeGraph.Domain/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeGraph.Domain
+{
+    public static partial class StringExtensions
+    {
+        public static IEnumerable<string> SplitStringOnCapitals(this string str)
+        {
+            Regex regex = SplitStringRegex();
+
+            return regex.Replace(str, "---").Split("---");
+        }
+
+        [GeneratedRegex(@"
+                (?<=[A-Z])(?=[A-Z][a-z]) |
+                 (?<=[^A-Z])(?=[A-Z]) |
+                 (?<=[A-Za-z])(?=[^A-Za-z])", RegexOptions.IgnorePatternWhitespace)]
+        private static partial Regex SplitStringRegex();
+    }
+}
