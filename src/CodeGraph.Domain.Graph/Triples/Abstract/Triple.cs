2441239119ae02c73d0030e5039d1f06e816931d
Chris Russell <cgrussell@gmail.com>
2024-04-20 08:20:25 +0100

    Added IEquality to other graph triples and relationships

--- src/CodeGraph.Domain.Graph/Triples/Abstract/Triple.cs
+++ src/CodeGraph.Domain.Graph/Triples/Abstract/Triple.cs
@@ -3,7 +3,7 @@ using CodeGraph.Domain.Graph.Relationships.Abstract;
 
 namespace CodeGraph.Domain.Graph.Triples.Abstract
 {
-    public abstract class Triple
+    public abstract class Triple : IEquatable<Triple>
     {
         protected Triple(Node nodeA, Node nodeB, Relationship relationship)
         {
@@ -12,16 +12,45 @@ namespace CodeGraph.Domain.Graph.Triples.Abstract
             Relationship = relationship;
         }
 
-        public Node NodeA { get; set; }
+        public Node NodeA { get; }
 
-        public Node NodeB { get; set; }
+        public Node NodeB { get; }
 
-        public Relationship Relationship { get; set; }
+        public Relationship Relationship { get; }
 
         public override string ToString()
         {
             return
                 $"MERGE (a:{NodeA.Label} {{ pk: \"{NodeA.Pk}\" }}) ON CREATE SET {NodeA.Set("a")} ON MATCH SET {NodeA.Set("a")} MERGE (b:{NodeB.Label} {{ pk: \"{NodeB.Pk}\" }}) ON CREATE SET {NodeB.Set("b")} ON MATCH SET {NodeB.Set("b")} MERGE (a)-[:{Relationship.Type}]->(b);";
+        }
+
+        public bool Equals(Triple? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return NodeA.Equals(other.NodeA) && NodeB.Equals(other.NodeB) && Relationship.Equals(other.Relationship);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == this.GetType() && Equals((Triple)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(NodeA, NodeB, Relationship);
+        }
+
+        public static bool operator ==(Triple? left, Triple? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Triple? left, Triple? right)
+        {
+            return !Equals(left, right);
         }
     }
 }
