9e2dda9722c67c6cfee34773e87707a463ae0db3
Chris Russell <cgrussell@gmail.com>
2024-05-04 17:42:37 +0100

    Extended ignore list

--- src/CodeGraph.Domain.Dotnet/CSharp/Walkers/CSharpMethodInvocationWalker.cs
+++ src/CodeGraph.Domain.Dotnet/CSharp/Walkers/CSharpMethodInvocationWalker.cs
@@ -70,8 +70,10 @@ namespace CodeGraph.Domain.Dotnet.CSharp.Walkers
 
             // Ignore dotnet's core methods
             if (invokedMethod.FullName.StartsWith("System", StringComparison.InvariantCultureIgnoreCase) || 
+                invokedMethod.FullName.StartsWith("Microsoft.Asp", StringComparison.InvariantCultureIgnoreCase) ||
                 invokedMethod.FullName.StartsWith("Microsoft.EntityFrameworkCore.Metadata", StringComparison.InvariantCultureIgnoreCase) ||
-                invokedMethod.FullName.StartsWith("Microsoft.EntityFrameworkCore.Metadata", StringComparison.InvariantCultureIgnoreCase) ||
+                invokedMethod.FullName.StartsWith("Microsoft.EntityFrameworkCore.Migrations", StringComparison.InvariantCultureIgnoreCase) ||
+                invokedMethod.FullName.StartsWith("Microsoft.Extensions", StringComparison.InvariantCultureIgnoreCase) ||
                 invokedMethod.FullName.StartsWith("Moq", StringComparison.InvariantCultureIgnoreCase)
                 ) return;
                 
