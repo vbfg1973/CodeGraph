a1ab74a7dc21cda9a5c75b13afc21ad4eca40556
Chris Russell <cgrussell@gmail.com>
2024-05-06 09:57:32 +0100

    Logging discovered method and property nodes in type definition walker

--- src/CodeGraph.Domain.Dotnet/CSharp/Walkers/CSharpTypeDefinitionWalker.cs
+++ src/CodeGraph.Domain.Dotnet/CSharp/Walkers/CSharpTypeDefinitionWalker.cs
@@ -52,6 +52,9 @@ namespace CodeGraph.Domain.Dotnet.CSharp.Walkers
             
             TypeNode typeNode = GetTypeNode(typeDeclarationSyntax);
             MethodNode methodNode = GetMethodNode(syntax);
+            
+            _logger.LogTrace("{Method} {SyntaxType} {NameFromSyntax} {FilePath} {MethodNodeFullName} {MethodNodeReturnType}", nameof(GetHasTriple), nameof(MethodDeclarationSyntax), syntax.Identifier.ToString(), syntax.SyntaxTree.FilePath, methodNode.FullName, methodNode.ReturnType);
+            
             _triples.Add(new TripleHas(typeNode, methodNode));
             _triples.AddRange(WordTriples(methodNode));
         }
@@ -64,6 +67,8 @@ namespace CodeGraph.Domain.Dotnet.CSharp.Walkers
             IPropertySymbol propertySymbol =
                 CSharpExtensions.GetDeclaredSymbol(_walkerOptions.DotnetOptions.SemanticModel, syntax)!;
             PropertyNode propertyNode = propertySymbol.CreatePropertyNode();
+            _logger.LogTrace("{Method} {SyntaxType} {NameFromSyntax} {FilePath} {PropertyNodeFullName} {PropertyNodeReturnType}", nameof(GetHasTriple), nameof(MethodDeclarationSyntax), syntax.Identifier.ToString(), syntax.SyntaxTree.FilePath, propertyNode.FullName, propertyNode.ReturnType);
+
             _triples.Add(new TripleHas(typeNode, propertyNode));
             _triples.AddRange(WordTriples(propertyNode));
         }
@@ -76,9 +81,11 @@ namespace CodeGraph.Domain.Dotnet.CSharp.Walkers
             IMethodSymbol methodSymbol =
                 CSharpExtensions.GetDeclaredSymbol(_walkerOptions.DotnetOptions.SemanticModel, syntax)!;
 
-            if (methodSymbol.TryGetInterfaceMethodFromImplementation(_walkerOptions.DotnetOptions.SemanticModel,
-                    out MethodNode interfaceMethodNode))
-                _triples.Add(new TripleImplementationOf(methodNode, interfaceMethodNode));
+            if (!methodSymbol.TryGetInterfaceMethodFromImplementation(_walkerOptions.DotnetOptions.SemanticModel,
+                    out MethodNode interfaceMethodNode)) return;
+            
+            _logger.LogTrace("{Method} {SyntaxType} {NameFromSyntax} {FilePath} {MethodNodeFullName} {MethodNodeReturnType}", nameof(GetHasTriple), nameof(MethodDeclarationSyntax), syntax.Identifier.ToString(), syntax.SyntaxTree.FilePath, interfaceMethodNode.FullName, interfaceMethodNode.ReturnType);
+            _triples.Add(new TripleImplementationOf(methodNode, interfaceMethodNode));
         }
     }
 }
